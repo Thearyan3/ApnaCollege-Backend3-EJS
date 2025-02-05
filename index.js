29963d656f4a25efa07e24537cebe9e01c01adf7
Aryan <chaudharyaryan3456@gmail.com>
2025-02-05 23:55:53 +0530

    completed Instagram Page

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express(); 
+const path = require("path");
+
+let port = 8080;
+
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`);
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/rollDice", (req, res) => {
+    let DiceValue = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", { DiceValue });
+});
+
+app.get("/ig/:username", (req, res) => {
+    let { username } = req.params;
+    const instaData = require("./data.json");
+    let data = instaData[username];
+    console.log(data);
+    res.render("Insta.ejs", { data });
+});
+
+app.get("/hello", (req, res) => {
+    res.send("hello");
+});
