7d7e0cd97e1c99625a19c047af881e498dc55bf4
Aryan <chaudharyaryan3456@gmail.com>
2025-02-06 13:18:28 +0530

    updated

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.listen(port, () => {
     console.log(`listening to the port ${port}`);
 });
 
+app.use(express.static(path.join(__dirname, "/public/css")));
+app.use(express.static(path.join(__dirname, "/public/js")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
